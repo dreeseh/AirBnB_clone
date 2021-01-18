a6f55beb04d2c7a953f84d8fcf4e46ad3633c1d5
dreeseh <1859@holbertonschool.com>
2021-01-18 16:37:58 +0000

    changed variable number to n

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -52,7 +52,7 @@ def number_template(n):
     """
     display a HTML page only if n is an integer
     """
-    return render_template('5-number.html', number=n)
+    return render_template('5-number.html', n=n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
