2866ab92003313f66d79181c0008e6bc12b3efbb
dreeseh <1859@holbertonschool.com>
2021-01-18 16:30:55 +0000

    added strict_slashes=False

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -48,7 +48,7 @@ def number(n):
     return "{:d} is a number".format(n)
 
 
-@app.route('/number_template/<int:n>')
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """
     display a HTML page only if n is an integer
