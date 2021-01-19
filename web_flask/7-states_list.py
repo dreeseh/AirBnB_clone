e0677384c1c155789b88f45680a92349e133bcf3
dreeseh <1859@holbertonschool.com>
2021-01-19 15:55:51 +0000

    task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+a script that starts a Flask web application
+"""
+from flask import Flask, render_template
+from models import storage
+from models import *
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """
+    display a HTML page sorted by name (A->Z)
+    """
+    states_all = sorted(list(storage.all("State").values()))
+    return (render_template('7-states_list.html', states_all=states_all))
+
+
+@app.teardown_appcontext
+def teardown():
+    """
+    After each request you must remove the current SQLAlchemy Session
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
