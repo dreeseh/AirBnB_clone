2ba5a75a0b0eb1cb07cd9844b12814bf68f92fc9
dreeseh <1859@holbertonschool.com>
2021-01-20 16:24:55 +0000

    task 11

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" starts a Web Flask application
+"""
+from flask import Flask, render_template
+from models import *
+from models import storage
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """ lisits cities & states in alpha order
+    """
+    states = storage.all("State").values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """ closes the storage
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0' port='5000')
