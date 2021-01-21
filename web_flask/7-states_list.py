123ed45a7215c381d19b7c5ff0ab2c5abd7e57f5
dreeseh <1859@holbertonschool.com>
2021-01-21 02:40:04 +0000

    changed strict slashes

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,23 +5,20 @@ a script that starts a Flask web application
 from flask import Flask, render_template
 from models import storage
 from models import *
-app.url_map.strict_slashes = False
 app = Flask(__name__)
 
 
-@app.route('/states_list')
+@app.route('/states_list', strict_slashes=False)
 def states_list():
-    """
-    display a HTML page sorted by name (A->Z)
+    """display a HTML page sorted by name (A->Z)
     """
     states_all = list(storage.all("State").values())
     return (render_template('7-states_list.html', states_all=states_all))
 
 
 @app.teardown_appcontext
-def teardown(self):
-    """
-    After each request you must remove the current SQLAlchemy Session
+def teardown(exception):
+    """After each request you must remove the current SQLAlchemy Session
     """
     storage.close()
 
