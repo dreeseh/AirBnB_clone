05568aeaadb5890417bf4c070539fceafacc1994
dreeseh <1859@holbertonschool.com>
2021-01-21 03:01:45 +0000

    redid def all

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,14 +10,14 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        if not cls:
-            return self.__objects
-        else:
-            new_obj = {obj: key for obj, key in self.__objects.items()
-                       if isinstance(key, cls)}
-            return new_obj
+        if cls is not None:
+            new_dict = {}
+            for key, value in self.__objects.items():
+                if cls == value.__class__ or cls == value.__class__.__name__:
+                    new_dict[key] = value
+            return new_dict
+        return self.__objects
 
-    def new(self, obj):
         """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
